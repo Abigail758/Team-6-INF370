3e509706581e43b107e12d8d7ceae288de6a4730
miyart <62016078+miyart@users.noreply.github.com>
2021-09-14 04:09:35 +0200

    employee/user

--- INF370Project6/INF370Project2/src/app/employees/add-user/add-user.component.ts
+++ INF370Project6/INF370Project2/src/app/employees/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  addform!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+  this.addform = this.formBuilder.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    idno: ['', Validators.required],
+    homeaddress: ['', Validators.required],
+    email: ['', Validators.required],
+    telno: [''],
+    cellno: ['', Validators.required],
+    emptype: ['', Validators.required],
+    });
   }
 
+  submit(form: any){
+    console.log('here')
+    console.log(form)
+    // var user = localStorage.getItem("usersList"); 
+    // var Data = JSON.parse(user);
+    // if(Data==null)
+    // {
+    //  var list: never[] = []
+    //   localStorage.setItem("usersList", JSON.stringify(list));
+    //   var user = localStorage.getItem("usersList");
+    //   Data = JSON.parse(user);
+    // }
+    // Data.push(form)
+    // localStorage.setItem("usersList", JSON.stringify(Data));
+    // this.router.navigate(["user"])
+  }
+
+  cancel(){
+    this.router.navigate(["user"])
+  }
 }
--- INF370Project6/INF370Project2/src/app/employees/user/user.component.ts
+++ INF370Project6/INF370Project2/src/app/employees/user/user.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface UserData {
+  firstname: String;
+  lastname: String;
+  title: String;
+  email: String;
+  cellno: String;
+  emptype: String;
+}
+
+const DATA: UserData[] = [{
+  firstname: "",
+  lastname: "",
+  title: "",
+  email: "",
+  cellno: "",
+  emptype: "",
+
+}]
+
 
 @Component({
-  selector: 'app-user',
+  selector: 'app-user', 
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
+
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ["firstname","email", "cellno", "emptype","delete" ];
+
+  dataSource = new MatTableDataSource(DATA);
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    var users = localStorage.getItem("usersList");
+    // var data = JSON.parse(users);
+    // this.dataSource = new MatTableDataSource(data);
   }
 
+  add(i: any){
+    this.router.navigate(['add-user'])
+  }
+
+  edit(i: any){
+    localStorage["ID"] = i;
+    this.router.navigate(['edit-user'])
+  }
+
+  removeAt(i: any) {
+    localStorage["userToDelete"] = i;
+
+    var users = localStorage.getItem("usersList");
+    // var Data = JSON.parse(users);
+    // Data.splice(i,1);
+
+    // this.dataSource = new MatTableDataSource(Data);
+    // localStorage.setItem("usersList", JSON.stringify(Data));
+  }
 }
